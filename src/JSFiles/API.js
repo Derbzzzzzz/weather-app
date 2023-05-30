c2cac2ef6d77ef8db20d642e85a0a5081395cd6b
Ryan Wahle <ryanwahle8@gmail.com>
2023-05-30 18:33:22 -0500

    Started Project with webpack and created basic API fxns for obtaining/processing data

---
+++ src/JSFiles/API.js
@@ -0,0 +1,47 @@
+async function retrieveData(city) {
+  try {
+    let response = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=f72389d70c86420a9da224039232805&q=" +
+        city,
+      { mode: "cors" }
+    );
+    let data = await response.json();
+    if (typeof data.location === "undefined") {
+      throw "Location not Found";
+    }
+    return data;
+  } catch (error) {
+    // console.log("Error: " + error);
+    return 1;
+  }
+}
+
+async function getData(city) {
+  let data = await retrieveData(city);
+
+  if (data == 1) {
+    console.log("Error!");
+    return;
+  }
+
+  // console.log(data.location)
+  // console.log(data.current)
+
+  let info = {};
+
+  info.name = data.location.name;
+  info.country = data.location.country;
+  info.CTemp = data.current.temp_c;
+  info.FTemp = data.current.temp_f;
+  info.FeelCTemp = data.current.feelslike_c;
+  info.FeelFTemp = data.current.feelslike_f;
+  info.humidity = data.current.humidity;
+
+  // console.log(info)
+
+  return info;
+}
+
+export {
+    getData
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./CSS/main.css";
+import {getData} from "./JSFiles/API.js"
+
+console.log(await getData("London"))
